b7d8b2c0169add2261b29235a1d7cbbc5401ca91
evanersisyan <nersisyan.e5@gmail.com>
2021-11-05 12:23:24 -0700

    added training.cpp

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main()
+{
+	std::cout<<"Zakary Evayin shat a sirum <3"<<std::endl;
+
+	return 0;
+}
---
+++ training.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	int a;
+	a=10;
+	std::cout<<a<<std::endl;
+
+	return 0;
+}
